dbb0c734e6c634d7aff0f3323a8a7256f8b2e004
ivan.huard <ivan.huard@epita.fr>
2025-01-07 15:59:38 +0100

    build: buildsystem added exec_ast and tests to buildsystem

--- src/42sh.c
+++ src/42sh.c
@@ -2,10 +2,12 @@
 
 #include "lexer/lexer.h"
 #include "parser/parser.h"
+#include "exec_ast/exec.h"
 
 int main()//int argc, char *argv[])
 {
     affiche("echo from lexer");
     print_parser();
+    exec_ast();
     return 0;
 }
---
+++ src/exec_ast/exec.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void exec_ast()
+{
+    printf("executing");
+}
