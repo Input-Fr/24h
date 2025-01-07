30ddac1f2c62a17f7abdb93bea6b3f85ebfd4216
ivan.huard <ivan.huard@epita.fr>
2025-01-07 10:56:39 +0100

    build: buildsystem

---
+++ src/42sh.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "lexer/lexer.h"
+#include "parser/parser.h"
+
+int main()//int argc, char *argv[])
+{
+    affiche("echo from lexer");
+    print_parser();
+    return 0;
+}
---
+++ src/lexer/lexer.h
@@ -0,0 +1,6 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+void affiche(char *str);
+
+#endif /* !LEXER_H */
