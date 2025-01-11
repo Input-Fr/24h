2369c0fbe024507944b841bb04855c55c24e43de
ivan.huard <ivan.huard@epita.fr>
2025-01-11 11:40:18 +0100

    chores: cleaning up

--- src/42sh.c
+++ src/42sh.c
@@ -1,3 +1,64 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lexer/lexer.h"
+#include "parser/parser.h"
+#include "parser/ast.h"
+
+
+static FILE * gere_usage(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        return stdin;
+    }
+    else
+    {
+        if (!strcmp(argv[1],"-c"))
+        {
+            if (argc <= 2)
+            {
+                return NULL;
+            }
+            return fmemopen(argv[2], (strlen(argv[2]) + 1),"r");
+        }
+        else
+        {
+            return fopen(argv[1],"r");
+        }
+    }
+
+}
+
+char lexer_file (FILE * hd)
+{
+    return fgetc(hd);
+}
+
+int main(int argc, char *argv[])
+{
+    FILE * value = gere_usage(argc,argv);
+    if (!value)
+    {
+        return -1;
+    }
+
+    // init lexer
+    struct lexer *lexer = lexer_new();
+    lexer->file = value;
+
+    // launch parser
+    enum parser_status status;
+    struct ast *ast = parse(&status, lexer);
+    (*ast->ftable->free)(ast);
+
+    return 0;
+}
+
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -52,11 +113,8 @@ int main(int argc, char *argv[])
     //const char* string = "if #world then\nhello";
     //const char* string = "if #world then hello";
     //const char* string = "if world then hello";
-    /*
+
     struct lexer *lex = lexer_new(string);
-    struct ast *ast;
-    enum parser_status status;
-    ast = parse(&status, lex);
     if (argc > 1)
     {
         if (strcmp(argv[1],"--print") == 0)
@@ -71,6 +129,7 @@ int main(int argc, char *argv[])
     }
 
     free(lex);
-    */
+
     return 0;
 }
+*/
