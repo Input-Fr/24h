bf6e7222d4abc5f9f63a2514164eb3254e454945
marcel.pecqueux <marcel.pecqueux@epita.fr>
2025-01-22 00:00:52 +0100

    fix: the run for ast

---
+++ src/ast/run2.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <err.h>
+
+#include "ast.h"
+#include "hash_map/hash_map.h"
+
+
+int for_run(struct ast * ast,struct hash_map *h)
+{
+	(void)(h);
+	(void)(ast);
+	return 0;
+}
